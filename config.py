c18fa79838b983237cbba5ba00e4c397848d89ee
= <=>
2014-10-09 13:40:53 -0700

    Init

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+Config file
+'''
+WINDOW_SIZE = 48
+NUM_CLUSTERS = 200
+LABEL_MAP = {'32' : '-1', '48' : '0', '49' : '1', '50' : '2', '51' : '3', '52' : '4', '53' : '5', '54' : '6', '55' : '7', '56' : '8', '57' : '9'}
+DELIMITER = ' '
+OUTPUT_DELIMITER = ','
+FREQUENCY_VALUE_BINS = 10 #for value discretization
+CENTER_FILE = 'centers.pkl.temp'
